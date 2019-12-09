1783af07dedd6dfe20aeda7aeaeb768f72dbd3b7
harlen lopez <harlen.d.lopez@gmail.com>
2019-12-09 12:55:37 -0800

    salmon-day 1

---
+++ JS/app.js
@@ -0,0 +1,76 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log('working');
+// firstDog = {
+//   weight: 35,
+//   breed: jindo,
+//   name: boog,
+//   imgUrl: 'https:',
+//   descriptionWords: ['hungry', 'blue eyes'],
+//   getDescription: function () {
+//     return this.descriptionWords[Math.floor(Math.random) * this.descriptionWords.length];
+//   }
+// };
+// firstcat = {
+//   weight: 4,
+//   breed: calico,
+//   name: mew,
+//   imgUrl: 'https'
+//   descriptionWords: ['small', 'meowy'],
+//   getDescription: function () {
+//     this.descriptionWords[Math.floor(math.random * this.descriptionWords.lenght)]
+//   }
+// }
+// var pets = [firstDog, firstcat];
+// var animalHolder = .getElemnetById('animal-holder');
+// animalHolder.textContent = 'the animals Go Here';
+// for (var i = 0, i < pets.length; i++){
+// var newParagraph = document.createElement('p');
+// newParagraph.textContent = 'an adoptable ${pets[i].breed} that is ${pets[i].getDescrition()}';
+// animalHolder.appendChild(newParagraph);
+// }
