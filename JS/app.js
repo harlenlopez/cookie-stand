7e51657872724164c61f368b09b604ec3dffb8ce
harlen lopez <harlen.d.lopez@gmail.com>
2019-12-10 12:46:47 -0800

    updated code

--- JS/app.js
+++ JS/app.js
@@ -25,12 +25,16 @@ var Tokyo = {
   minCustomer: 3,
   maxCustomer: 24,
   avgSales: 1.2,
+  hourlyArray: [],
   randomCustomer: function () {
     return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
   },
 
   estimeSales: function () {
-    return this.estimeSales() * this.avgSales;
+    for (var i = 0; i < hours.length; i++) {
+      console.log(hours[i]);
+      this.hourlyArray[i] = Math.floor(this.randomCustomer() * this.avgSales);
+    }
   }
 };
 
@@ -39,12 +43,16 @@ var Dubai = {
   minCustomer: 11,
   maxCustomer: 38,
   avgSales: 3.7,
+  hourlyArray: [],
   randomCustomer: function () {
     return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
   },
 
   estimeSales: function () {
-    return this.estimeSales() * this.avgSales;
+    for (var i = 0; i < hours.length; i++) {
+      console.log(hours[i]);
+      this.hourlyArray[i] = Math.floor(this.randomCustomer() * this.avgSales);
+    }
   }
 };
 
@@ -53,12 +61,16 @@ var Paris = {
   minCustomer: 20,
   maxCustomer: 38,
   avgSales: 2.3,
+  hourlyArray: [],
   randomCustomer: function () {
     return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
   },
 
   estimeSales: function () {
-    return this.estimeSales() * this.avgSales;
+    for (var i = 0; i < hours.length; i++) {
+      console.log(hours[i]);
+      this.hourlyArray[i] = Math.floor(this.randomCustomer() * this.avgSales);
+    }
   }
 };
 
@@ -67,12 +79,16 @@ var Lima = {
   minCustomer: 2,
   maxCustomer: 16,
   avgSales: 4.6,
+  hourlyArray: [],
   randomCustomer: function () {
     return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
   },
 
   estimeSales: function () {
-    return this.estimeSales() * this.avgSales;
+    for (var i = 0; i < hours.length; i++) {
+      console.log(hours[i]);
+      this.hourlyArray[i] = Math.floor(this.randomCustomer() * this.avgSales);
+    }
   }
 };
 
@@ -81,7 +97,7 @@ function hoursAppender(saleAmount) {
   for (var i = 0; i < saleAmount.length; i++) {
     var saleAmountHolder = document.getElementById('city-holder');
     var newLi = document.createElement('li');
-    newLi.textContent = `${saleAmount}: ${Seattle} cookies`;   
+    newLi.textContent = `${saleAmount}: ${Seattle} cookies`;
     console.log(saleAmount);
     var newLi1 = document.createElement('li');
     newLi1.textContent = `${hours[i]}: ${saleAmount[i]} cookies`;
@@ -90,17 +106,16 @@ function hoursAppender(saleAmount) {
   }
   return runningTotal;
 }
-console.log(Seattle);
+// console.log(Seattle);
 
-function createHourlyLi(hourlyArray) {
-  var cities = [Seattle, Tokyo, Dubai, Paris, Lima];
+function createHourlyLi(city) {
   var cityHolder = document.getElementById('city-holder');
   for (var i = 0; i < cities.length; i++) {
     var newLi = document.createElement('li');
-    var newLi1 = document.createElement('li');
     newLi.textContent = `${cities[i].name}`;
     cityHolder.appendChild(newLi);
-    var total = hoursAppender(hourlyArray);
+    var total = hoursAppender(city.hourlyArray);
+    var newLi1 = document.createElement('li');
     newLi1.textContent = `Total: ${total} cookies`;
     cityHolder.appendChild(newLi1);
   }
@@ -108,6 +123,52 @@ function createHourlyLi(hourlyArray) {
 
 // Create City and Hourly List
 Seattle.estimeSales();
+createHourlyLi(Seattle);
 
-createHourlyLi(Seattle.hourlyArray);
+Tokyo.estimeSales();
+createHourlyLi(Tokyo);
 
+Paris.estimeSales();
+createHourlyLi(Paris);
+
+Dubai.estimeSales();
+createHourlyLi(Dubai);
+
+Lima.estimeSales();
+createHourlyLi(Lima);
+
+
+
+// // constructor
+// //tend to be a singular noun
+// function Pet(breed, age, disposition) {
+//   //set key b=vaue pairs on the new object
+//   this.breed = 'breed';
+//   this.age = 'age';
+//   this.disposition = 'disposition';
+// }
+
+// Pet.prototype.getDisposition = function() {
+//   var randomIndex = Math.floor(Math.random() * this.getDescription.length);
+//   return this.descriptionWords[randomIndex];
+// };
+
+// Pet.prototype.render = function(){
+//   var animalTable = document.getElementById('animal-table');
+//   var petRow = document.createElement('tr');
+//   var breedCell = document.createElement('td');
+//   breedCell.textContent = this.breed;
+
+  
+
+//   var weigthCell = document.getElement('td'){
+//     weightCell.textContent = this.breed;
+//     petRow.appendChild(breedCell);
+//   }
+
+// var firstDog = new Pet('jindo', 35, 'good boy');
+// var firstCt = new Pet('calico', 4, 'purrrrfect');
+// var iguana = new Pet('green', 'cslly', 'cuddly');
+
+// var pets = [firstCt, firstDog, iguana];
+// pets[i] = 
