9fa3ae881fd9f5eaa6caa75454cced908a2c116d
harlen lopez <harlen.d.lopez@gmail.com>
2019-12-09 19:13:51 -0800

    cities added and working with hours

--- JS/app.js
+++ JS/app.js
@@ -1,49 +1,110 @@
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+console.log('js');
+
+var Seattle = {
+  name: 'Seattle',
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgSales: 6.3,
+  hourlyArray: [],
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+  },
+
+  estimeSales: function () {
+    for (var i = 0; i < hours.length; i++) {
+      console.log(hours[i]);
+      Seattle.hourlyArray[i] = Math.floor(this.randomCustomer() * Seattle.avgSales);
+    }
+    // console.log(this.hourlyArray);
+    // return this.estimeSales() * this.avgSales;
+  }
+};
+Seattle.estimeSales();
+function hoursAppender(hours) {
+  for (var i = 0; i < hours.length; i++) {
+    var hoursHolder = document.getElementById('hours');
+    var newLi = document.createElement('li');
+    newLi.textContent = `hours: ${Seattle[i].name} cookies`;   
+    console.log(hours);
+     var newLi = document.createElement('li');
+    newLi.textContent = `${hours[i]}`;
+    hoursHolder.appendChild(newLi);
+  }
+}
+console.log(Seattle);
+
+var Tokyo = {
+  name: 'Tokyo',
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgSales: 1.2,
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+  },
+
+  estimeSales: function () {
+    return this.estimeSales() * this.avgSales;
+  }
+};
+
+var Dubai = {
+  name: 'Dubai',
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgSales: 3.7,
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+  },
+
+  estimeSales: function () {
+    return this.estimeSales() * this.avgSales;
+  }
+};
+
+var Paris = {
+  name: 'Paris',
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgSales: 2.3,
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+  },
+
+  estimeSales: function () {
+    return this.estimeSales() * this.avgSales;
+  }
+};
+
+var Lima = {
+  name: 'Lima',
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgSales: 4.6,
+  randomCustomer: function () {
+    return Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+  },
+
+  estimeSales: function () {
+    return this.estimeSales() * this.avgSales;
+  }
+};
+
+
+var cities = [Seattle, Tokyo, Dubai, Paris, Lima];
+var cityHolder = document.getElementById('city-holder');
+for (var i = 0; i < cities.length; i++) {
+  // console.log(cities);
+  var newLi = document.createElement('li');
+  newLi.textContent = `${cities[i].name}`;
+  //console.log(cities[i]);
+  cityHolder.appendChild(newLi);
+  hoursAppender(Seattle.hourlyArray);
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
+console.log(cities);
 
 // console.log('working');
 // firstDog = {
@@ -65,12 +126,4 @@
 //   getDescription: function () {
 //     this.descriptionWords[Math.floor(math.random * this.descriptionWords.lenght)]
 //   }
-// }
-// var pets = [firstDog, firstcat];
-// var animalHolder = .getElemnetById('animal-holder');
-// animalHolder.textContent = 'the animals Go Here';
-// for (var i = 0, i < pets.length; i++){
-// var newParagraph = document.createElement('p');
-// newParagraph.textContent = 'an adoptable ${pets[i].breed} that is ${pets[i].getDescrition()}';
-// animalHolder.appendChild(newParagraph);
-// }
+// };
